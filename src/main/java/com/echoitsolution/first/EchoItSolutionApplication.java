553eb15acc1cad7e0782dbeb24531ca702e0fe17
Rahul Taneja <rahul.taneja@aithentic.com>
2025-08-17 20:20:01 +0530

    initial commit

---
+++ src/main/java/com/echoitsolution/first/EchoItSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.echoitsolution.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoItSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoItSolutionApplication.class, args);
+	}
+
+}
