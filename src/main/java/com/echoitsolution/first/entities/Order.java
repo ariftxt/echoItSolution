c24f49132f5d53d1ef2bf65cdfbd829ee0cbbce7
ariftxt <arif.txt@gmail.com>
2025-08-27 16:30:45 +0530

    added annotation in entity

--- src/main/java/com/echoitsolution/first/entities/Order.java
+++ src/main/java/com/echoitsolution/first/entities/Order.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.Date;
 
+@AllArgsConstructor
+@Data
 @Entity
-@Table(name = "Order")
+@Table(name = "orders")
 public class Order {
 
     @Id
@@ -27,41 +31,4 @@ public class Order {
 
     private Long createdBy;
 
-    public Order(Long id, String name, Integer quantity, String type, String status, Date createdOn, Long createdBy) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-        this.type = type;
-        this.status = status;
-        this.createdOn = createdOn;
-        this.createdBy = createdBy;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    public Long getCreatedBy() {
-        return createdBy;
-    }
 }
